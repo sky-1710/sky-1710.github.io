d3fec0267973c5cf9834302998448f26cd9eef79
sky17-10 <sky1710@g.skku.edu>
2025-09-28 01:25:37 +0900

    feat: Fixed new setting 8

--- _plugins/category_generator.rb
+++ _plugins/category_generator.rb
@@ -1,4 +1,7 @@
-# _plugins/category_generator.rb
+require "set"
+
+Jekyll.logger.info "CategoryGenerator", "start"
+
 module Jekyll
   class CategoryPage < Page
     def initialize(site, base, dir, category_title, posts)
@@ -7,11 +10,10 @@ module Jekyll
       @dir  = dir
       @name = "index.html"
 
-      self.process(@name)
-      self.read_yaml(File.join(base, "_layouts"), "category.html")
+      process(@name)
+      read_yaml(File.join(base, "_layouts"), "category.html")
       self.data["title"] = category_title
       self.data["posts"] = posts
-      # permalink를 명시적으로 고정 (중복 방지)
       self.data["permalink"] = "/" + dir + "/"
     end
   end
@@ -21,20 +23,22 @@ module Jekyll
     priority :low
 
     def generate(site)
-      buckets = {} # { "대학교/2025s/3-2/programming-language" => {name: "대학교 / 2025s / 3-2 / programming language", posts: [...] } }
+      buckets = {} # { "대학교/2025s/3-2/programming-language" => { "name" => "...", "posts" => [...] } }
 
       site.posts.docs.each do |post|
         cats = Array(post.data["categories"])
         next if cats.empty?
 
-        slug = cats.map { |c| Jekyll::Utils.slugify(c) }.join("/")
+        slug  = cats.map { |c| Jekyll::Utils.slugify(c, mode: "raw") }.join("/")
         title = cats.join(" / ")
         buckets[slug] ||= { "name" => title, "posts" => [] }
         buckets[slug]["posts"] << post
       end
 
-      # 이미 같은 URL의 페이지가 있으면 만들지 않음(중복 가드)
+      Jekyll.logger.info "CategoryGenerator", "bucket_count=#{buckets.keys.size}"
+
       existing_urls = site.pages.map { |p| p.url }.to_set
+      created = 0
 
       buckets.each do |slug, data|
         dir = File.join("categories", slug)
@@ -42,7 +46,11 @@ module Jekyll
         next if existing_urls.include?(url)
 
         site.pages << CategoryPage.new(site, site.source, dir, data["name"], data["posts"])
+        created += 1
+        Jekyll.logger.info "CategoryGenerator", "created #{url} (#{data['posts'].size} posts)"
       end
+
+      Jekyll.logger.info "CategoryGenerator", "done, created=#{created}"
     end
   end
 end
